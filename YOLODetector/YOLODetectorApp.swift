f0e0afd3486271b316f287fc3b96c9e0c8c9f1ed
TusharW4ni <reachtusharwani@gmail.com>
2025-11-10 00:23:38 -0600

    Initial Commit

---
+++ YOLODetector/YOLODetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  YOLODetectorApp.swift
+//  YOLODetector
+//
+//  Created by Tushar Wani on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct YOLODetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
